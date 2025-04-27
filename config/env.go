995d084e557bd70f21cfde64b231015b93c02c33
Juraj Siroky <siroky.juraj0@gmail.com>
2025-04-27 02:57:25 +0200

    feat: loaded environment variables from .env file

---
+++ config/env.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Host       string
+	Port       string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+var Envs = initConfig()
+
+func initConfig() Config {
+	godotenv.Load()
+
+	return Config{
+		Host:       getEnv("HOST", "http://localhost"),
+		Port:       getEnv("PORT", "8080"),
+		DBUser:     getEnv("DB_USER", "root"),
+		DBPassword: getEnv("DB_PASSWORD", "mypass"),
+		DBName:     getEnv("DB_NAME", "ecom"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
