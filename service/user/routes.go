bfe47de159f505454a81da8d12ec28c9b0e2ccac
Juraj Siroky <siroky.juraj0@gmail.com>
2025-04-27 02:54:29 +0200

    feat: added login and register routes to user service

---
+++ service/user/routes.go
@@ -0,0 +1,23 @@
+package user
+
+import "net/http"
+
+type Handler struct {
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *http.ServeMux) {
+	router.HandleFunc("POST /login", h.handleLogin)
+	router.HandleFunc("POST /register", h.handleRegister)
+}
+
+func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+
+}
